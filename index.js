1760b4221c45bede1871a6c112459be0f4eee4f7
DylanPage212 <dylanpage212@gmail.com>
2021-03-23 21:07:59 -0700

    Finish first 3 tasks

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
+const worldcupfinal2014 = fifaData.filter(function(item) {
 
+    return item.Year === 2014 && item.Stage === "Final";
+});
 //(a) Home Team name for 2014 world cup final
+
+    console.log("Task 1a", worldcupfinal2014[0]["Home Team Name"]);
 
 //(b) Away Team name for 2014 world cup final
 
+    console.log("Task 1b", worldcupfinal2014[0]["Away Team Name"]);
+
 //(c) Home Team goals for 2014 world cup final
+
+    console.log("Task 1c", worldcupfinal2014[0]["Home Team Goals"]);
 
 //(d) Away Team goals for 2014 world cup final
 
+    console.log("Task 1d", worldcupfinal2014[0]["Away Team Goals"]);
+
 //(e) Winner of 2014 world cup final */
 
+    console.log("Task 1e", worldcupfinal2014[0]["Win conditions"])
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use getFinals to do the following:
@@ -24,9 +36,15 @@ Use getFinals to do the following:
 hint - you should be looking at the stage key inside of the objects
 */
 
-function getFinals(/* code here */) {
-   /* code here */
+function getFinals(data) {
+   const finalstage = data.filter(function(item){
+       return item.Stage === "Final";
+   }) 
+   return finalstage;
 }
+
+console.log(getFinals(fifaData));
+
 
 
 
@@ -36,11 +54,15 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(array, callback) {
+    
+    const years = callback(array).map(function(items){
+        return items.Year;
+    })
+    return years;
 }
 
-
+console.log(getYears(fifaData, getFinals));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Use the higher-order function getWinners to do the following:  
@@ -140,3 +162,7 @@ export default{
     getWinnersByYear,
     getAverageGoals
 }
+
+
+
+
